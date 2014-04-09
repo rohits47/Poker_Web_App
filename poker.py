9203c7512fba3dddb3d9489e1a2fbb38eda8b875
Rohit Sanbhadti <rohits47@gmail.com>
2014-04-09 15:20:26 -0500

    initial structure

---
+++ poker.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
+from engine import Table,Deck,Player
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/poker.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+	d = Deck()
+	p1 = Player("rohit")
+	table1 = Table()
+	lst = d.getHand()
+	return str(lst) + str(len(d.deck))
+	return "This is the poker home page."
+
+if __name__ == '__main__':
+	app.run(debug=True)
